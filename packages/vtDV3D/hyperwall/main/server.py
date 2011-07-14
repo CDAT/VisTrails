a651526f2798200f97048e76228ce27daa2b19ca
Thomas Maxwell <thomas.maxwell@nasa.gov>
2011-07-14 16:35:30 -0400

    Hyperwall fix.  Please enter the commit message for your changes. Lines starting

--- packages/vtDV3D/hyperwall/main/server.py
+++ packages/vtDV3D/hyperwall/main/server.py
@@ -4,8 +4,13 @@ Created on Jun 15, 2011
 @author: tpmaxwel
 '''
 import sys, os
-from vtUtilities import *
-
 if __name__ == '__main__':
-    optionsDict = {  'hw_role'  : 'server' }
-    executeVistrail( 'HyperwallWorkflow', options=optionsDict )
+    from userpackages.vtDV3D import executeVistrail
+#    optionsDict = {  'hw_role'  : 'none' }
+    optionsDict = {  'hw_role': 'server', 'debug': 'False' } #, 'hw_nodes': 'localhost' }
+    try:
+        executeVistrail( 'DemoWorkflow8', options=optionsDict )
+#        executeVistrail( options=optionsDict )
+    except Exception, err:
+        print " executeVistrail exception: %s " % str( err )
+
