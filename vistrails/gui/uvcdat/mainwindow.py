158a5980688bda4fb28b6a56b0991cc8aef0079d
Emanuele Santos <emanuelesantos@gmail.com>
2011-10-19 11:45:30 -0300

    Merged Charles changes into VisTrails repository:  - Replaced designer files with Charles' updates to Variable Properties panel.  - Added Calculator as a dockable panel.

--- vistrails/gui/uvcdat/mainwindow.py
+++ vistrails/gui/uvcdat/mainwindow.py
@@ -1,4 +1,8 @@
 from PyQt4 import QtCore, QtGui
+import os
+import cdms2
+import vcs
+import __main__
 
 from gui.uvcdat.ui_mainwindow import Ui_MainWindow
 from gui.uvcdat.workspace import Workspace
@@ -7,6 +11,7 @@ from gui.uvcdat.dockplot import DockPlot
 from gui.uvcdat.dockvariable import DockVariable
 from gui.uvcdat.variable import VariableProperties
 from gui.uvcdat.plot import PlotProperties
+from gui.uvcdat.dockcalculator import DockCalculator
 
 from packages.spreadsheet.spreadsheet_controller import spreadsheetController
 import gui.uvcdat.uvcdat_rc
@@ -16,22 +21,97 @@ import gui.uvcdat.uvcdat_rc
 #from packages.spreadsheet.spreadsheet_window import SpreadsheetWindow
 #from packages.spreadsheet.spreadsheet_tabcontroller import StandardWidgetTabController 
 
+from qtbrowser import customizeVCDAT
+from qtbrowser import commandsRecorderWidget
+from qtbrowser import preferencesWidget
+from qtbrowser import mainMenuWidget
+from qtbrowser import mainToolbarWidget
+
 class UVCDATMainWindow(QtGui.QMainWindow):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None,customPath=None,styles=None):
         super(UVCDATMainWindow, self).__init__(parent)
+        self.root=self
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setDocumentMode(True)
+        #init user options
+        self.initCustomize(customPath,styles)
+        self.root = self
+        self.tool_bar = mainToolbarWidget.QMainToolBarContainer(self)
+        self.canvas=[]
+        for i in range(4):
+            self.canvas.append(vcs.init())
+        # Create the command recorder widget
+        self.recorder = commandsRecorderWidget.QCommandsRecorderWidget(self)
+        #Adds a shortcut to the record function
+        self.record = self.recorder.record
+        self.preferences = preferencesWidget.QPreferencesDialog(self)
+        self.preferences.hide()
+        ###########################################################
+        # Init Menu Widget
+        ###########################################################
+        self.mainMenu = mainMenuWidget.QMenuWidget(self)
         self.embedSpreadsheet()
         self.createDockWindows()
         self.createViewActions()
         self.connectSignals()
+
+    def initCustomize(self,customPath,styles):
+        if customPath is None:
+            customPath=os.path.join(os.environ["HOME"],"PCMDI_GRAPHICS","customizeVCDAT.py")
+            
+        if os.path.exists(customPath):
+            execfile(customPath,customizeVCDAT.__dict__,customizeVCDAT.__dict__)
+
+        if styles is None:
+            styles=customizeVCDAT.appStyles
+            
+        icon = QtGui.QIcon(customizeVCDAT.appIcon)
+        self.setWindowIcon(icon)
+
+        ## cdms2 setup section
+        cdms2.axis.time_aliases+=customizeVCDAT.timeAliases
+        cdms2.axis.level_aliases+=customizeVCDAT.levelAliases
+        cdms2.axis.latitude_aliases+=customizeVCDAT.latitudeAliases
+        cdms2.axis.longitude_aliases+=customizeVCDAT.longitudeAliases
+        cdms2.setNetcdfShuffleFlag(customizeVCDAT.ncShuffle)
+        cdms2.setNetcdfDeflateFlag(customizeVCDAT.ncDeflate)
+        cdms2.setNetcdfDeflateLevelFlag(customizeVCDAT.ncDeflateLevel)
         
+        ## StylesSheet
+        st=""
+        if isinstance(styles,str):
+            st = styles
+        elif isinstance(styles,dict):
+            for k in styles.keys():
+                val = styles[k]
+                if isinstance(val,QtGui.QColor):
+                    val = str(val.name())
+                st+="%s:%s; " % (k,val)
+        if len(st)>0: self.setStyleSheet(st)
+
+        ###########################################################
+        ###########################################################
+        ## Prettyness
+        ###########################################################
+        ###########################################################
+        #self.setGeometry(0,0, 1100,800)
+        self.setWindowTitle('The Ultrascale Visualization Climate Data Analysis Tools - (UV-CDAT)')
+        ## self.resize(1100,800)
+        #self.setMinimumWidth(1100)
+        self.main_window_placement()
+        
+    def main_window_placement(self):
+        screen = QtGui.QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width()-size.width())/4, (screen.height()-size.height())/5)
+
     def createDockWindows(self):
         self.workspace = Workspace()
-        self.dockTemplate = DockTemplate()
-        self.dockPlot = DockPlot()
-        self.dockVariable = DockVariable()
+        self.dockTemplate = DockTemplate(self)
+        self.dockPlot = DockPlot(self)
+        self.dockVariable = DockVariable(self)
+        self.dockCalculator = DockCalculator(self)
         
         self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.workspace)
         self.tabifyDockWidget(self.workspace, self.dockTemplate)
@@ -39,12 +119,14 @@ class UVCDATMainWindow(QtGui.QMainWindow):
         self.workspace.raise_()
 
         self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.dockVariable)
+        self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.dockCalculator)
 
     def createViewActions(self):
         self.ui.menuView.addAction(self.workspace.toggleViewAction())
         self.ui.menuView.addAction(self.dockTemplate.toggleViewAction())
         self.ui.menuView.addAction(self.dockPlot.toggleViewAction())
         self.ui.menuView.addAction(self.dockVariable.toggleViewAction())
+        self.ui.menuView.addAction(self.dockCalculator.toggleViewAction())
         
     def connectSignals(self):
         self.ui.actionExit.triggered.connect(self.quit)
@@ -52,6 +134,12 @@ class UVCDATMainWindow(QtGui.QMainWindow):
     def quit(self):
         #FIXME
         #ask to save projects
+        print "quiting"
+        if self.preferences.confirmB4Exit.isChecked():
+            # put here code to confirm exit
+            pass
+        if self.preferences.saveB4Exit.isChecked():
+            self.preferences.saveState()
         QtGui.QApplication.instance().quit()
         
     def showVariableProperties(self):
@@ -75,3 +163,48 @@ class UVCDATMainWindow(QtGui.QMainWindow):
     def cleanup(self):
         self.setCentralWidget(QtGui.QWidget())
         self.spreadsheetWindow.setParent(None)
+
+    def stick_defvar_into_main_dict(self,var):
+        __main__.__dict__[var.id]=var
+
+    def stick_main_dict_into_defvar(self,results=None):
+        #First evaluate if there's any var in the result
+        res = None
+        if results is not None:
+            tmp = __main__.__dict__[results]
+        else:
+            tmp=None
+        added = []
+        remove =[]
+        if isinstance(tmp,cdms2.tvariable.TransientVariable):
+            __main__.__dict__[tmp.id]=tmp
+            added.append(tmp.id)
+            res = tmp.id
+        elif tmp is not None:
+            self.processList(tmp,added)
+            
+        if results is not None:
+            del(__main__.__dict__[results])
+        for k in __main__.__dict__:
+            if isinstance(__main__.__dict__[k],cdms2.tvariable.TransientVariable):
+                if __main__.__dict__[k].id in added and k!=__main__.__dict__[k].id:
+                    remove.append( __main__.__dict__[k].id)
+                    res = k
+                if not k in remove:
+                    __main__.__dict__[k].id=k
+                    self.dockVariable.widget().addVariable(__main__.__dict__[k])
+        for r in remove:
+            del(__main__.__dict__[r])
+
+        return res
+    
+    def processList(self,myList,added):
+        for v in myList:
+            if isinstance(v,cdms2.tvariable.TransientVariable):
+                __main__.__dict__[v.id]=v
+                added.append(v.id)
+            elif isinstance(v,(list,tuple)):
+                self.processList(v,added)
+            elif isinstance(v,dict):
+                self.processList(dict.values(),added)
+        return
--- vistrails/gui/uvcdat/variable.py
+++ vistrails/gui/uvcdat/variable.py
@@ -1,35 +1,359 @@
 from PyQt4 import QtCore, QtGui
 
-from gui.uvcdat.ui_variable import Ui_VariableProperties
+import os
+import cdms2
+
 from qtbrowser.esgf import QEsgfBrowser
 from qtbrowser.commandLineWidget import QCommandLine
-class VariableProperties(QtGui.QWidget):
+from qtbrowser import axesWidgets
+from qtbrowser import vcdatCommons
+from qtbrowser import customizeVCDAT
+
+class VariableProperties(QtGui.QDialog):
+    FILTER = "CDAT data (*.cdms *.ctl *.dic *.hdf *.nc *.xml)"
+
+    FILETYPE = {'CDAT': ['cdms', 'ctl', 'dic', 'hdf', 'nc', 'xml']}
+        
     def __init__(self, parent=None):
         super(VariableProperties, self).__init__(parent)
-        self.ui = Ui_VariableProperties()
-        self.ui.setupUi(self)
-        self.root = self
+        v=QtGui.QVBoxLayout()
+        l=QtGui.QLabel("Load From")
+        v.addWidget(l)
+        self.originTabWidget=QtGui.QTabWidget(self)
+        sp = QtGui.QSplitter(QtCore.Qt.Vertical)
+        sp.addWidget(self.originTabWidget)
+        self.dims=QtGui.QFrame()
+        sp.addWidget(self.dims)
+        v.addWidget(sp)
+        h=QtGui.QHBoxLayout()
+        s=QtGui.QSpacerItem(40,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Maximum)
+        h.addItem(s)
+        self.btnDefine=QtGui.QPushButton("Define")
+        h.addWidget(self.btnDefine)
+        self.btnDefineAs=QtGui.QPushButton("Define As")
+        h.addWidget(self.btnDefineAs)
+        self.btnCancel=QtGui.QPushButton("Cancel")
+        h.addWidget(self.btnCancel)
+        v.addLayout(h)
+        self.layout=v
+        self.setLayout(v)
+        self.parent=parent
+        self.root = parent.root
+        self.createFileTab()
         self.createESGFTab()
         self.createCalculatorTab()
+        self.createDimensions()
         self.connectSignals()
         
-    @classmethod
-    def instance(klass):
-        if not hasattr(klass, '_instance'):
-            klass._instance = klass()
-        return klass._instance
+    ## @classmethod
+    ## def instance(klass):
+    ##     if not hasattr(klass, '_instance'):
+    ##         klass._instance = klass()
+    ##     return klass._instance
 
     def connectSignals(self):
-        self.ui.btnCancel.clicked.connect(self.close)
+        self.btnCancel.clicked.connect(self.close)
+        self.tbOpenFile.clicked.connect(self.openSelectFileDialog)
+        self.connect(self.fileEdit, QtCore.SIGNAL('returnPressed()'),
+                     self.updateFile)
+        self.connect(self.historyList, QtCore.SIGNAL('itemClicked(QListWidgetItem *)'),
+                     self.selectFromList)
+        self.connect(self.bookmarksList, QtCore.SIGNAL('itemClicked(QListWidgetItem *)'),
+                     self.selectFromList)
+        ## FIXME: signal when dropping to bookmarks so it sticks for next time
+        self.connect(self.varCombo, QtCore.SIGNAL('currentIndexChanged(const QString&)'),
+                     self.variableSelected)
+
+        ## Define button
+        self.btnDefine.clicked.connect(self.defineVarClicked)
+        self.connect(self,QtCore.SIGNAL('definedVariableEvent'),self.root.dockVariable.widget().addVariable)
+        self.connect(self.bookmarksList,QtCore.SIGNAL("droppedInto"),self.droppedBookmark)
+
+
+    def droppedBookmark(self,event):
+        text = str(event.mimeData().text())
+        self.addBookmark(text)
+
+    def addBookmark(self,txt):
+        duplicate=False
+        for i in range(self.bookmarksList.count()):
+            it = self.bookmarksList.item(i)
+            if it.text()==txt:
+                duplicate=True
+                break
+            
+        if duplicate is False:
+            self.bookmarksList.addItem(txt)
+            customizeVCDAT.fileBookmarks.append(txt)
+            
+    def createFileTab(self):
+        #Top Part
+        ## File Select Section
+        v=QtGui.QVBoxLayout()
+        h=QtGui.QHBoxLayout()
+        l=QtGui.QLabel("File")
+        h.addWidget(l)
+        self.fileEdit=QtGui.QLineEdit()
+        h.addWidget(self.fileEdit)
+        self.tbOpenFile=QtGui.QToolButton()
+        self.tbOpenFile.setText('...')
+        self.tbOpenFile.setToolTip('View and select files')
+        h.addWidget(self.tbOpenFile)
+        v.addLayout(h)
+
+        ## Variable Select part
+        h=QtGui.QHBoxLayout()
+        l=QtGui.QLabel("Variable(s):")
+        l.setSizePolicy(QtGui.QSizePolicy.Maximum,QtGui.QSizePolicy.Preferred)
+        h.addWidget(l)
+        self.varCombo=QtGui.QComboBox()
+        self.varCombo.setSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Fixed)
+        h.addWidget(self.varCombo)
+        v.addLayout(h)
+
+        ## Bottom Part
+        h=QtGui.QHBoxLayout()
+        l=QtGui.QLabel("History:")
+        self.historyList=vcdatCommons.QDragListWidget(type="history")
+        self.historyList.setSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Preferred)
+        self.historyList.setAlternatingRowColors(True)
+        h.addWidget(l)
+        h.addWidget(self.historyList)
+        l=QtGui.QLabel("Bookmarks:")
+        self.bookmarksList=vcdatCommons.QDragListWidget(type="bookmarks",dropTypes=["history"])
+        self.bookmarksList.setSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Preferred)
+        self.bookmarksList.setAlternatingRowColors(True)
+        self.bookmarksList.setSortingEnabled(True)
+        for f in customizeVCDAT.fileBookmarks:
+            self.addBookmark(f)
+            
+        h.addWidget(l)
+        h.addWidget(self.bookmarksList)
+        v.addLayout(h)
         
+        f=QtGui.QFrame()
+        f.setLayout(v)
+
+        self.originTabWidget.addTab(f,"File")
+
     def createESGFTab(self):
-        layout = QtGui.QVBoxLayout()
-        self.esgfBrowser = QEsgfBrowser(self)
-        layout.addWidget(self.esgfBrowser)
-        self.ui.tabESGF.setLayout(layout)
+        ## layout = QtGui.QVBoxLayout()
+        ## self.esgfBrowser = QEsgfBrowser(self)
+        ## layout.addWidget(self.esgfBrowser)
+        self.originTabWidget.addTab(QEsgfBrowser(self),"ESGF")
     
     def createCalculatorTab(self):
-        layout = QtGui.QVBoxLayout()
         self.calculator = QCommandLine(self)
-        layout.addWidget(self.calculator)
-        self.ui.tabCalc.setLayout(layout)
+        self.originTabWidget.addTab(self.calculator,"Existing")
+
+
+    def createDimensions(self):
+        self.dimsLayout=QtGui.QVBoxLayout()
+        l=QtGui.QLabel("Dimensions")
+        self.dimsLayout.addWidget(l)
+        self.dims.setLayout(self.dimsLayout)
+        
+    def openSelectFileDialog(self):
+        file = QtGui.QFileDialog.getOpenFileName(self, 'Open CDAT data file...',
+                                                 self.root.dockVariable.lastDirectory,
+                                                 VariableProperties.FILTER + ';;All files (*.*)')
+        if not file.isNull():
+            self.setFileName(file)
+
+    def setFileName(self,fnm):
+        self.fileEdit.setText(fnm)
+        self.updateFile()
+
+    def updateFile(self):
+        fnm = self.fileEdit.text()
+        fi = QtCore.QFileInfo(fnm)
+        ft = str(fi.suffix())
+        for i in range(self.historyList.count()):
+            it = self.historyList.item(i)
+            if it.text()==fnm:
+                self.historyList.takeItem(i)
+                break
+        self.historyList.insertItem(0,fnm)
+        self.historyList.setCurrentRow(0)
+        # I imagine that there will be filetypes that both ParaView and
+        # CDAT will know how to deal with them.
+        if fi.exists() or fn[:7]=="http://":
+            if fi.exists():
+            	self.root.dockVariable.lastDirectory=str(fi.dir().path())
+            self.emit(QtCore.SIGNAL('fileChanged'), str(fnm))
+            other_list = []
+            for name, types in VariableProperties.FILETYPE.iteritems():
+                if ft in types:
+                    if name == 'CDAT':
+                        self.updateCDMSFile(str(fnm))
+
+                    if name not in other_list:
+                        other_list.append(name)
+            
+            self.updateOtherPlots(other_list)
+        else:
+            self.emit(QtCore.SIGNAL('fileChanged'), None)
+
+    def selectFromList(self,item):
+        self.setFileName(str(item.text()))
+        
+    def updateCDMSFile(self, fn):
+        if fn[:7]=="http://":
+            ## Maybe add something for my proxy errors here?
+            self.cdmsFile = cdms2.open(fn)
+            self.root.record("## Open file: %s" % fn)
+            self.root.record("cdmsFile = cdms2.open('%s')" % fn)
+        else:
+            self.cdmsFile = cdms2.open(fn)
+            self.root.record("## Open file: %s" % fn)
+            self.root.record("cdmsFile = cdms2.open('%s')" % fn)
+        self.updateVariableList()
+        
+    def updateOtherPlots(self, namelist):
+        self.emit(QtCore.SIGNAL('updateOtherPlots'), namelist)
+        
+    def updateVariableList(self):
+        self.varCombo.clear()
+        if self.cdmsFile!=None:
+            # Add Variables sorted based on their dimensions
+            curDim = -1
+            for (dim, varId) in sorted([(len(var.listdimnames()), var.id)
+                                        for var in self.cdmsFile.variables.itervalues()]):
+                if dim!=curDim:
+                    curDim = dim
+                    count = self.varCombo.count()
+                    self.varCombo.insertSeparator(count)
+                    self.varCombo.model().item(count, 0).setText('%dD VARIABLES' % dim)
+                var = self.cdmsFile.variables[varId]
+                varName = var.id + ' ' + str(var.shape) + ' ['
+                
+                if hasattr(var, 'long_name'):
+                    varName += var.long_name
+                if hasattr(var, 'units') and var.units!='':
+                    if varName[-1]!='[': varName += ' '
+                    varName += var.units
+                varName += ']'
+                self.varCombo.addItem(varName, QtCore.QVariant(QtCore.QStringList(['variables', var.id])))
+
+            # Add Axis List
+            count = self.varCombo.count()
+            self.varCombo.insertSeparator(count)
+            self.varCombo.model().item(count, 0).setText('AXIS LIST')
+            for axis in self.cdmsFile.axes.itervalues():
+                axisName = axis.id + " (" + str(len(axis)) + ") - [" + axis.units + ":  (" + str(axis[0]) + ", " + str(axis[-1]) + ")]"                
+                self.varCombo.addItem(axisName, QtCore.QVariant(QtCore.QStringList(['axes', axis.id])))
+
+            # By default, select first var
+            self.varCombo.setCurrentIndex(1)
+            
+    def variableSelected(self, varName):
+        if varName == '':
+            return
+
+#        self.defineVarButton.setEnabled(not varName.isNull()) # Enable define button
+        varName = str(varName).split()[0]
+        # Send signal to setup axisList in 'quickplot' tab
+        self.root.record("## Open a variable in file")
+        self.root.record("cdmsFileVariable = cdmsFile['%s']" % varName)
+
+        # Create and setup the axislist
+        axisList = axesWidgets.QAxisList(self.cdmsFile, varName, self)
+        axisList.setupVariableAxes()
+        N=self.dimsLayout.count()
+        while N>1:
+            it = self.dimsLayout.takeAt(N-1)
+            it.widget().destroy()
+            del(it)
+            self.dims.update()
+            N=self.dimsLayout.count()
+        self.dimsLayout.addWidget(axisList)
+        ## FIXME
+        #self.updateVarInfo(axisList)
+        
+    def updateVarInfo(self, axisList):
+        """ Update the text box with the variable's information """
+        if axisList is None:
+            return
+        
+        var = axisList.getVar()
+        varInfo = ''
+        for line in var.listall():
+            varInfo += line + '\n'
+##        print "TEXT:",varInfo
+##        self.varInfoWidget.setText(varInfo)
+    def defineVarClicked(self,*args):
+        self.getUpdatedVarCheck()
+
+    def getUpdatedVarCheck(self,targetId=None):
+        """ Return a new tvariable object with the updated information from
+        evaluating the var with the current user selected args / options
+        """
+        axisList = self.dimsLayout.itemAt(1).widget()
+
+        if targetId is not None:
+            tid = targetId
+        elif axisList.cdmsFile is None:
+            tid = axisList.var.id
+        else:
+            tid = axisList.var
+
+        ## ## Ok at that point we need to figure out if 
+        ## if self.tabWidget.tabExists(tid) and targetId is None:
+        ##     self.ask.setTextValue(tid)
+        ##     self.ask.show()
+        ##     self.ask.exec_()
+        ## else:
+        ##     self.getUpdatedVar(tid)
+        self.getUpdatedVar(tid)
+
+    def getUpdatedVar(self,targetId):
+        axisList = self.dimsLayout.itemAt(1).widget()
+        kwargs = self.generateKwArgs()
+        # Here we try to remove useless keywords as we record them
+        cmds = ""
+        for k in kwargs:
+            if k=='order':
+                o = kwargs[k]
+                skip = True
+                for i in range(len(o)):
+                    if int(o[i])!=i:
+                        skip = False
+                        break
+                if skip:
+                    continue
+            cmds += "%s=%s," % (k, repr(kwargs[k]))
+        cmds=cmds[:-1]
+        updatedVar = axisList.getVar()(**kwargs)
+
+        # Get the variable after carrying out the: def, sum, avg... operations
+        updatedVar = axisList.execAxesOperations(updatedVar)
+        self.root.record("## Defining variable in memory")
+
+        if axisList.cdmsFile is None:
+            oid = updatedVar.id
+        else:
+            oid = "cdmsFileVariable"
+        updatedVar.id = targetId
+        self.root.record("%s = %s(%s)" % (targetId,oid,cmds))
+        ## Squeeze?
+        if self.root.preferences.squeeze.isChecked():
+            updatedVar=updatedVar(squeeze=1)
+            self.root.record("%s = %s(squeeze=1)" % (targetId,targetId))
+
+        self.emit(QtCore.SIGNAL('definedVariableEvent'),updatedVar)
+        return updatedVar
+    
+    def generateKwArgs(self, axisList=None):
+        """ Generate and return the variable axes keyword arguments """
+        if axisList is None:
+            axisList = self.dimsLayout.itemAt(1).widget()
+
+        kwargs = {}        
+        for axisWidget in axisList.getAxisWidgets():
+            if not axisWidget.isHidden():
+                kwargs[axisWidget.axis.id] = axisWidget.getCurrentValues()
+
+        # Generate additional args
+        #kwargs['squeeze'] = 0
+        kwargs['order'] = axisList.getAxesOrderString()
+        return kwargs
