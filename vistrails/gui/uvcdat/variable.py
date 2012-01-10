CDAT/VisTrails

commit 63da4a1e8a0fb3cc903168bd21a231dcd1b59b11

Improve ROI selection gui.

vistrails/gui/uvcdat/variable.py
@@ -240,14 +240,15 @@ class VariableProperties(QtGui.QDockWidget):
         labelLayout.addWidget(l)
         
         self.selectRoiButton = QtGui.QPushButton('Select ROI', self)
+        self.selectRoiButton.setMinimumSize( 50, 50 )
         labelLayout.addWidget( self.selectRoiButton )        
         self.connect( self.selectRoiButton, QtCore.SIGNAL('clicked(bool)'), self.selectRoi )        
         self.roiSelector = ROISelectionDialog( self.parent )
         self.connect(self.roiSelector, QtCore.SIGNAL('doneConfigure()'), self.setRoi )
         if self.roi: self.roiSelector.setROI( self.roi )
         
-        self.dimsLayout.addLayout( labelLayout )
         self.dims.setLayout( self.dimsLayout )
+        self.dimsLayout.addLayout( labelLayout )
 
     def selectRoi( self ): 
         if self.roi: self.roiSelector.setROI( self.roi )

vistrails/gui/uvcdat/roiSelector.py
@@ -196,6 +196,7 @@ class ROISelectionDialog(QDialog):
         layout.addWidget(self.view)
         layout.addWidget(w)
         self.setLayout(layout)
+        self.view.scale( 0.4, 0.4 )
         
     def setROI( self, roi ):
         geoPt0 = QPointF( roi[0], roi[1] )

vistrails/packages/vtDV3D/ROISelection.py
@@ -169,6 +169,7 @@ class ROISelectionDialog(QDialog):
         ROIcorner1 = QPointF(360, 90)   if self.lonRangeType == 0 else QPointF( 180, 90 )  
         worldMapExtent = WorldMapGridExtent[ self.lonRangeType ]      
         self.view = GraphicsView( item, worldMapExtent, ROIcorner0, ROIcorner1, self )
+        self.view.setMinimumSize( 500, 500 )
         self.filename = QString()
         self.view.setScene(self.scene)
 #        self.scene.addItem(item)
@@ -275,6 +276,7 @@ class ROISelectionDialog(QDialog):
         panelLayout.addWidget(self.cancelButton)
         self.connect(self.okButton, SIGNAL('clicked(bool)'), self.okTriggered)
         self.connect(self.cancelButton, SIGNAL('clicked(bool)'), self.close )
+        self.view.scale( 0.4, 0.4 )
 
 
 #        self.renderROIButton = QPushButton("Render ROI")
@@ -298,6 +300,9 @@ class ROISelectionDialog(QDialog):
         layout.addWidget(self.view)
         layout.addWidget(w)
         self.setLayout(layout)
+        
+    def initScaling(self):
+        self.view.scale( 0.4, 0.4 )
         
 #        autoLoad = nvSettings.data['autoLoad Default Field']
 #        if autoLoad:
@@ -768,7 +773,12 @@ class MainForm(QDialog):
 
     def setRoi(self):
         self.roi = self.roiSelector.getROI()
-        self.roiLabel.setText( "ROI: %s" % str( self.roi )  )  
+        self.roiLabel.setText( "ROI: %s" % str( self.roi )  ) 
+        
+#    def show(self):
+#        self.roiSelector.initScaling()
+#        QDialog.show( self )
+         
         
 if __name__ == '__main__':                                                
     app = QApplication(sys.argv)

commit 8607437834a5c63acba2dfe0cd23407f248d3715

bugfix: Removed bug introduced in commit 63da4a1 that prevented a variable from a local file to be loaded.
vistrails/gui/uvcdat/varaible.py
   In VariableProperties.getUpdatedVar()
         setting the url only when the cdmsFile has one (it seems that local files do not have the attribute uri)

vistrails/gui/uvcdat/variable.py
@@ -526,7 +526,10 @@ class VariableProperties(QtGui.QDockWidget):
                 if k != 'order':
                     kwargs_str += "%s=%s," % (k, repr(v))
             return kwargs_str
-        cdmsVar = CDMSVariable(filename=self.cdmsFile.id, url=self.cdmsFile.uri, name=targetId,
+        url = None
+        if hasattr(self.cdmsFile, "uri"):
+            url = self.cdmsFile.uri
+        cdmsVar = CDMSVariable(filename=self.cdmsFile.id, url=url, name=targetId,
                                axes=get_kwargs_str(kwargs))
         controller.add_defined_variable(cdmsVar)
         # controller.add_defined_variable(self.cdmsFile.id,targetId,kwargs)