6a9f22653581777e024186a958a166461db39588
Jorge Poco <jpocom@gmail.com>
2012-07-02 10:08:14 -0400

    Menu option to select different kind of eco-regions

--- vistrails/gui/uvcdat/mainMenuWidget.py
+++ vistrails/gui/uvcdat/mainMenuWidget.py
@@ -19,6 +19,7 @@ import commandsRecorderWidget
 import customizeUVCDAT
 import genutil,cdutil
 import preFunctionPopUpWidget
+from regionExtractor import *
 class QMenuWidget(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self,parent)
@@ -86,10 +87,18 @@ class QMenuWidget(QtGui.QWidget):
                       "OCT","NOV","DEC"]:
                 m.addAction(s)
             self.connect(m,QtCore.SIGNAL("triggered(QAction *)"),self.seasons)
+        
+        self.spatial = self.pcmdiTools.addMenu("Spatial Tools")
+        extract = self.spatial.addMenu("Extract")
+        extract.addAction("Transcom Regions")
+        extract.addAction("Koeppen-Geiger")
+        extract.addAction("Dominant \"pure\" PFT")
+        extract.addAction("Vegetated Mask")
+        self.connect(extract,QtCore.SIGNAL("triggered(QAction *)"),self.extractRegions)
 
         stats = self.pcmdiTools.addMenu("Statistics")
         stats.setTearOffEnabled(True)
-        self.statsFuncs = {"Mean" : {"func":cdutil.averager,"nargsMin":1,"nargsMax":2},
+        self.statsFuncs = {"Mean" : {"func":cdutil.averager,"nargsMin":1,"nargsMax":2, "choices":[("action", ["average", "sum"])]},
                            "Variance" :{"func":genutil.statistics.variance,"nargsMin":1,"nargsMax":2,"choices":["centered","biased",],"entries":["max_pct_missing"]},
                            "Standard Deviation" : {"func":genutil.statistics.std,"nargsMin":1,"nargsMax":2,"choices":["centered","biased",],"entries":["max_pct_missing"]},
                            "Root Mean Square" : {"func":genutil.statistics.rms,"nargsMin":2,"nargsMax":3,"choices":["centered","biased",],"entries":["max_pct_missing"]},
@@ -264,4 +273,49 @@ class QMenuWidget(QtGui.QWidget):
             prj_controller = get_current_project_controller()
             prj_controller.change_defined_variable_time_bounds(v.id, vtnm) 
 
-                
+    def extractRegions(self,action):
+        nm = str(action.text())
+        
+        import gui
+        rec = "## Computing"
+        if nm == "Transcom Regions":
+            func = gui.uvcdat.regionExtractor.REGIONS_1
+            funcnm = 'gui.uvcdat.regionExtractor.REGIONS_1'
+        elif nm == "Koeppen-Geiger":
+            func = gui.uvcdat.regionExtractor.REGIONS_2;
+            funcnm = 'gui.uvcdat.regionExtractor.REGIONS_2'
+        elif nm == "Dominant \"pure\" PFT":
+            func = gui.uvcdat.regionExtractor.REGIONS_3
+            funcnm = 'gui.uvcdat.regionExtractor.REGIONS_3'
+        elif nm == "Vegetated Mask":
+            func = gui.uvcdat.regionExtractor.REGIONS_4
+            funcnm = 'gui.uvcdat.regionExtractor.REGIONS_4'
+        self.bDialog = QtGui.QInputDialog()
+        
+        regions = func.getRegions();
+        region,ok = self.bDialog.getItem(self, nm, 'Choose Region: ', regions, editable=False);
+        if ok is False:
+            return
+        region = str(region)
+        vtdesc = nm
+        rec += nm.lower()
+        selectedVars=self.root.dockVariable.widget().getSelectedDefinedVariables()
+        for v in selectedVars:
+            #create new variable
+            tmp = func(v, region)
+            ext = "".join(region.lower().split())
+            newid = "%s_%s" % (v.id,ext)
+            tmp.id = newid
+            
+            #save new variable
+            self.root.dockVariable.widget().addVariable(tmp)
+            self.root.record(rec)
+            self.root.record("%s = %s(%s, '%s')" % (newid, funcnm, v.id, str(regions)))
+            
+            #send command to project controller to be stored as provenance
+            from api import get_current_project_controller
+            prj_controller = get_current_project_controller()
+            vtfuncnm = "%s(%s, '%s')"%(funcnm,v.id, region)
+            prj_controller.calculator_command([v.id], vtdesc, vtfuncnm, newid)
+            
+
---
+++ vistrails/gui/uvcdat/regionExtractor.py
@@ -0,0 +1,69 @@
+import cdms2
+import os
+from cdms2.MV2 import *
+import scipy.io
+
+packagePath = os.path.dirname( __file__ )  
+defaultMaskDir = os.path.join( packagePath, 'resources/mask' )
+
+class RegionExtractor:
+    """
+    RegionExtractor is the class that ...
+    """
+    
+    def __init__(self, type):
+        self.type = type;
+        if type=='Transcom Regions':
+            name='_mask_transcom.mat'
+        elif type=='Koeppen-Geiger':
+            name='_mask_koeppen.mat'
+        elif type=='Dominant \"pure\" PFT':
+            name='_mask_hurtt_synmap.mat'
+        elif type=='Vegetated Mask':
+            name='_mask_vegetated.mat'
+        else:
+            return
+        
+        filename = os.path.join(defaultMaskDir, name)
+        # read regions from file
+        self.file = scipy.io.loadmat(filename)
+        code=self.file['code']
+        self.regions = {}
+        for r in range(code.shape[0]):
+            if type=='Vegetated Mask':
+                self.regions[str(code[r,0][0])]=r
+            else:
+                self.regions[str(code[r,0][0])]=r+1
+        
+    def __call__(self, _var, region):
+        var = _var.clone()
+        if not region in self.regions.keys():
+            return None
+        
+        val = self.regions[region]
+        # reading mask data
+        regions_data = self.file['mask']
+        regions_var  = cdms2.createVariable(
+                         ones(regions_data.shape),
+                         grid = cdms2.createUniformGrid(89.75, 360, -0.5, -180, 720, 0.5),
+                         mask = where(equal(regions_data, val), 0, 1))
+        
+        new_mask_var = regions_var.regrid(var.getGrid())
+        new_mask = getmask(new_mask_var)
+ 
+        if var.mask <> None:
+            var.mask = logical_or(var.mask, new_mask)
+        else:
+            var.mask = new_mask;
+            
+        return var
+    
+    def getRegions(self):
+        if self.type <> None:
+            return self.regions.keys()
+        return None
+
+REGIONS_1 = RegionExtractor('Transcom Regions')
+REGIONS_2 = RegionExtractor('Koeppen-Geiger')
+REGIONS_3 = RegionExtractor('Dominant \"pure\" PFT')
+REGIONS_4 = RegionExtractor('Vegetated Mask')
