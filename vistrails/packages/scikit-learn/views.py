18a148a43cae85402b0b89a7524ddcb27f4b521c
Jorge Poco <jpocom@gmail.com>
2012-08-07 10:35:10 -0400

    show labels per instance

--- vistrails/packages/scikit-learn/views.py
+++ vistrails/packages/scikit-learn/views.py
@@ -2,9 +2,10 @@ from core.modules.vistrails_module import Module, NotCacheable
 from core.modules.basic_modules import Integer, Float, String, List
 from packages.NumSciPy.Array import NDArray
 from packages.spreadsheet.basic_widgets import SpreadsheetCell
-from packages.spreadsheet.spreadsheet_cell import QCellWidget
+from packages.spreadsheet.spreadsheet_cell import QCellWidget, QCellToolBar
 from PyQt4 import QtGui
 from matplotlib.widgets import  RectangleSelector
+from packages.vtDV3D.DV3DCell import *
 
 from core.bundles import py_import
 try:
@@ -15,11 +16,14 @@ try:
     pylab = py_import('pylab', mpl_dict)
 except Exception, e:
     debug.critical("Exception: %s" % e)
+    
+packagePath = os.path.dirname( __file__ )
 
 ################################################################################
 
 class ProjectionWidget(QCellWidget):
-    """
+    """ ProjectionWidget is a widget to show 2D projections. It has some interactive
+    features like, show labels, selections, and synchronization.
     """
     
     def __init__(self, parent=None):
@@ -32,15 +36,23 @@ class ProjectionWidget(QCellWidget):
         self.setLayout(centralLayout)
         centralLayout.setMargin(0)
         centralLayout.setSpacing(0)
+        
         self.figManager = None
         self.rectSelector = None
+        self.showLabels = False
+        self.inputPorts = None;
+        
+        self.toolBarType = QProjectionToolBar
 
-    def updateContents(self, inputPorts):
+    def updateContents(self, inputPorts=None):
         """ updateContents(inputPorts: tuple) -> None
         Update the widget contents based on the input data
         """
+        if inputPorts is not None: self.inputPorts = inputPorts
+        
         # draw in which canvas?
-        self.draw(inputPorts)
+#        self.inputPorts = inputPorts;
+        self.draw()
         newFigManager = pylab.get_current_fig_manager()
         
         if self.figManager != None:
@@ -69,8 +81,8 @@ class ProjectionWidget(QCellWidget):
         self.figManager = newFigManager
         self.update()
         
-    def draw(self, inputPorts):
-        (_matrix, title, _colors, labels) = inputPorts
+    def draw(self):
+        (_matrix, title, _colors, labels) = self.inputPorts
         matrix = _matrix.array
         colors = _colors.array if _colors is None else 'b'
         
@@ -85,8 +97,19 @@ class ProjectionWidget(QCellWidget):
         pylab.scatter( matrix[:,0], matrix[:,1], 
                        c=colors, cmap=pylab.cm.Spectral, 
                        marker='o')
-
+  
+        print 'DRAWWWWW: ', self.showLabels
+        if self.showLabels and labels is not None:
+            for label, x, y in zip(labels, matrix[:, 0], matrix[:, 1]):
+                pylab.annotate(
+                    str(label), 
+                    xy = (x, y), 
+                    xytext = (5, 5),
+                    textcoords = 'offset points',
+                    bbox = dict(boxstyle = 'round,pad=0.2', fc = 'yellow', alpha = 0.5),
+                )
         
+        # selections
         self.rectSelector = RectangleSelector(pylab.gca(), 
                                               self.onselect, 
                                               drawtype='box', 
@@ -157,3 +180,37 @@ class parallelcoordinates(Module):
     """
     """
     pass
+
+###############################################################################
+class QCellToolBarShowLabels(QtGui.QAction):
+    """
+    QCellToolBarShowLabels is the action to show labels per instance.
+    """
+    def __init__(self, parent=None):
+        """ QCellToolBarShowLabels(parent: QWidget)
+                                         -> QCellToolBarExportTimeSeries
+        Setup the image, status tip, etc. of the action
+        """
+        QtGui.QAction.__init__(self,
+                               QtGui.QIcon(os.path.join( packagePath,  'icons/labels.png' )),
+                               "&Show Labels",
+                               parent)
+        self.setStatusTip("Show labels per instance")
+        
+    def triggeredSlot(self, checked=False):
+        cellWidget = self.toolBar.getSnappedWidget()
+        cellWidget.showLabels = not cellWidget.showLabels
+        cellWidget.updateContents()
+
+class QProjectionToolBar(QCellToolBar):
+    """
+    QProjectionToolBar derives from QCellToolBar to give the ...
+    a customizable toolbar
+    """
+    def createToolBar(self):
+        """ createToolBar() -> None
+        This will get call initially to add customizable widgets
+        """
+        QCellToolBar.createToolBar(self)
+        self.appendAction(QCellToolBarShowLabels(self))
+        
