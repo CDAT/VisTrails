905aa5f62c4d3f73df4e002758fa32a965df9ead
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-09-13 23:50:51 -0400

    Added missing files

---
+++ vistrails/packages/pvclimate/pvsubmit_dialog.py
@@ -0,0 +1,49 @@
+
+# Qt modules
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import QObject, pyqtSlot
+
+# Local modules
+from ui_pvsubmit_job import Ui_Dialog
+
+# System modules
+import os
+
+#
+class PVSubmitFileDialog(QtGui.QDialog, Ui_Dialog):
+   #--------------------------------------------------------------------------
+   def __init__(self, parent=None):
+       super(PVSubmitFileDialog, self).__init__(parent)
+       self.setupUi(self)
+       self.files = []
+       self.connect(self.remoteBrowserButton, QtCore.SIGNAL("clicked(bool)"),
+         self.open_remote_browser)
+       self.connect(self.localBrowserButton, QtCore.SIGNAL("clicked(bool)"),
+         self.open_local_browser)
+
+   def accept(self):
+      print 'I am clicked'
+      print 'input path is ', self.inputPath.text()
+      print 'output path is ', self.outputPath.text()
+      print 'queue is ', self.queueLineEdit.text()
+      self.done(QtGui.QDialog.Accepted)
+
+   def open_remote_browser(self):
+     import gui.uvcdat.pvFileDialog as fd
+     fileDialog = fd.PVFileDialog(self)
+     if fileDialog.exec_():
+       for file in fileDialog.getAllSelectedFiles():
+         self.files.append(str(file))
+       self.inputPath.setText(self.files[0])
+
+   def open_local_browser(self):
+     directory = os.path.expanduser(unicode(self.outputPath.text()))
+     fd = QtGui.QFileDialog(self, 'Select: Output Directory', directory)
+     fd.setFileMode(QtGui.QFileDialog.DirectoryOnly)
+     if fd.exec_() == 1:
+       dir = str(fd.selectedFiles().first())
+       self.outputPath.setText(dir)
+
+   def get_remote_files(self):
+     return self.files
+
---
+++ vistrails/packages/pvclimate/ui_pvsubmit_job.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'pvsubmit_job.ui'
+#
+# Created: Wed Sep 12 13:28:49 2012
+#      by: PyQt4 UI code generator 4.9.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(580, 378)
+        self.gridLayout = QtGui.QGridLayout(Dialog)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.label = QtGui.QLabel(Dialog)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.localBrowserButton = QtGui.QToolButton(Dialog)
+        self.localBrowserButton.setObjectName(_fromUtf8("localBrowserButton"))
+        self.gridLayout.addWidget(self.localBrowserButton, 1, 2, 1, 1)
+        self.buttonBox = QtGui.QDialogButtonBox(Dialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.gridLayout.addWidget(self.buttonBox, 4, 1, 1, 1)
+        self.inputPath = QtGui.QLineEdit(Dialog)
+        self.inputPath.setObjectName(_fromUtf8("inputPath"))
+        self.gridLayout.addWidget(self.inputPath, 0, 1, 1, 1)
+        self.remoteBrowserButton = QtGui.QToolButton(Dialog)
+        self.remoteBrowserButton.setObjectName(_fromUtf8("remoteBrowserButton"))
+        self.gridLayout.addWidget(self.remoteBrowserButton, 0, 2, 1, 1)
+        self.outputPath = QtGui.QLineEdit(Dialog)
+        self.outputPath.setObjectName(_fromUtf8("outputPath"))
+        self.gridLayout.addWidget(self.outputPath, 1, 1, 1, 1)
+        self.label_2 = QtGui.QLabel(Dialog)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem, 3, 1, 1, 1)
+        self.queueLineEdit = QtGui.QLineEdit(Dialog)
+        self.queueLineEdit.setObjectName(_fromUtf8("queueLineEdit"))
+        self.gridLayout.addWidget(self.queueLineEdit, 2, 1, 1, 1)
+        self.label_3 = QtGui.QLabel(Dialog)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+
+        self.retranslateUi(Dialog)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), Dialog.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("Dialog", "Input Directory", None, QtGui.QApplication.UnicodeUTF8))
+        self.localBrowserButton.setText(QtGui.QApplication.translate("Dialog", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.remoteBrowserButton.setText(QtGui.QApplication.translate("Dialog", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("Dialog", "Output Directory", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("Dialog", "Submit Queue", None, QtGui.QApplication.UnicodeUTF8))
+
