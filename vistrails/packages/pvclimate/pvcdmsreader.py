8466dcaa958e95d86fa93318a20f4d7e9da2b3b8
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-12-11 14:02:57 -0500

    Adding reader for CDMS variable

---
+++ vistrails/packages/pvclimate/pvcdmsreader.py
@@ -0,0 +1,11 @@
+
+class PVCDMSReader():
+    def __init__(self):
+        pass
+    
+    
+    def convert(self, cdms_var):
+        '''Convert a cdms data to vtk image data
+        
+        '''
+        
