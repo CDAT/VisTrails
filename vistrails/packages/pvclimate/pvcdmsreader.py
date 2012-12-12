e783e24f43cc31c4f198278ef3d51783493bb471
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-12-11 22:37:39 -0500

    Work in progress

--- vistrails/packages/pvclimate/pvcdmsreader.py
+++ vistrails/packages/pvclimate/pvcdmsreader.py
@@ -16,10 +16,121 @@ class PVCDMSReader():
         self.grid_bounds = None
         self.zscale = 1
         self.output_type = None
+        self.lon = None
+        self.lat = None
+        self.lev = None
+        self.time = None
         pass
     
-    def get_grid_specs(self, var_data, grid_bounds, zscale):
-        pass
+    def isLevelAxis(self, axis):
+        if axis.isLevel(): return True
+        # @note: What's so special about isobaric?
+        if ( axis.id == 'isobaric' ): 
+            axis.designateLevel(1)
+            return True
+        return False
+    
+    def getCoordType(self, axis):
+        icoord = -2
+        if axis.isLongitude():
+            self.lon = axis
+            icoord  = 0
+        if axis.isLatitude():
+            self.lat = axis
+            icoord  = 1
+        if self.isLevelAxis(axis):
+            self.lev = axis
+            icoord  = 2
+        # @todo: Not sure if this is needed here
+        if axis.isTime():
+            self.time = axis
+            icoord  = 2
+        return icoord
+    
+    def getAxisValues( self, axis, roi ):
+        values = axis.getValue()
+        bounds = None
+        if roi:
+            if   axis.isLongitude():  bounds = [ roi[0], roi[2] ]
+            elif axis.isLatitude():   bounds = [ roi[1], roi[3] ] 
+        if bounds:
+            if axis.isLongitude() and (values[0] > values[-1]):
+               values[-1] = values[-1] + 360.0 
+            value_bounds = [ min(values[0],values[-1]), max(values[0],values[-1]) ]
+            mid_value = ( value_bounds[0] + value_bounds[1] ) / 2.0
+            mid_bounds = ( bounds[0] + bounds[1] ) / 2.0
+            offset = (360.0 if mid_bounds > mid_value else -360.0)
+            trans_val = mid_value + offset
+            if (trans_val > bounds[0]) and (trans_val < bounds[1]):
+                value_bounds[0] = value_bounds[0] + offset
+                value_bounds[1] = value_bounds[1] + offset           
+            bounds[0] = max( [ bounds[0], value_bounds[0] ] )
+            bounds[1] = min( [ bounds[1], value_bounds[1] ] )
+        return bounds, values
+    
+    def newList( self, size, init_value ):
+        return [ init_value for i in range(size) ]       
+    
+    def get_grid_specs(self, var, roi, zscale):        
+        gridOrigin = self.newList( 3, 0.0 )
+        outputOrigin = self.newList( 3, 0.0 )
+        gridBounds = self.newList( 6, 0.0 )
+        gridSpacing = self.newList( 3, 1.0 )
+        gridExtent = self.newList( 6, 0 )
+        outputExtent = self.newList( 6, 0 )
+        gridShape = self.newList( 3, 0 )
+        gridSize = 1        
+        self.lev = var.getLevel()
+        axis_list = var.getAxisList()
+        for axis in axis_list:
+            size = len( axis )
+            iCoord = self.getCoordType( axis )
+            roiBounds, values = self.getAxisValues( axis, roi )
+            if iCoord >= 0:
+                iCoord2 = 2*iCoord
+                gridShape[ iCoord ] = size
+                gridSize = gridSize * size
+                outputExtent[ iCoord2+1 ] = gridExtent[ iCoord2+1 ] = size-1                    
+                if iCoord < 2:
+                    lonOffset = 0.0 #360.0 if ( ( iCoord == 0 ) and ( roiBounds[0] < -180.0 ) ) else 0.0
+                    outputOrigin[ iCoord ] = gridOrigin[ iCoord ] = values[0] + lonOffset
+                    spacing = (values[size-1] - values[0])/(size-1)
+                    if roiBounds:
+                        if ( roiBounds[1] < 0.0 ) and  ( roiBounds[0] >= 0.0 ): roiBounds[1] = roiBounds[1] + 360.0
+                        gridExtent[ iCoord2 ] = int( round( ( roiBounds[0] - values[0] )  / spacing ) )                
+                        gridExtent[ iCoord2+1 ] = int( round( ( roiBounds[1] - values[0] )  / spacing ) )
+                        if gridExtent[ iCoord2 ] > gridExtent[ iCoord2+1 ]:
+                            geTmp = gridExtent[ iCoord2+1 ]
+                            gridExtent[ iCoord2+1 ] = gridExtent[ iCoord2 ] 
+                            gridExtent[ iCoord2 ] = geTmp
+                        outputExtent[ iCoord2+1 ] = gridExtent[ iCoord2+1 ] - gridExtent[ iCoord2 ]
+                        outputOrigin[ iCoord ] = lonOffset + roiBounds[0]
+                    roisize = gridExtent[ iCoord2+1 ] - gridExtent[ iCoord2 ] + 1                  
+                    gridSpacing[ iCoord ] = spacing
+                    gridBounds[ iCoord2 ] = roiBounds[0] if roiBounds else values[0] 
+                    gridBounds[ iCoord2+1 ] = (roiBounds[0] + roisize*spacing) if roiBounds else values[ size-1 ]
+                else:                                             
+                    gridSpacing[ iCoord ] = 1.0
+#                    gridSpacing[ iCoord ] = zscale
+                    gridBounds[ iCoord2 ] = values[0]  # 0.0
+                    gridBounds[ iCoord2+1 ] = values[ size-1 ] # float( size-1 )
+        if gridBounds[ 2 ] > gridBounds[ 3 ]:
+            tmp = gridBounds[ 2 ]
+            gridBounds[ 2 ] = gridBounds[ 3 ]
+            gridBounds[ 3 ] = tmp
+        gridSpecs = {}
+        md = { 'datasetId' : self.datasetId,  'bounds':gridBounds, 'lat':self.lat, 'lon':self.lon, 'lev':self.lev, 'time': self.timeAxis }
+        gridSpecs['gridOrigin'] = gridOrigin
+        gridSpecs['outputOrigin'] = outputOrigin
+        gridSpecs['gridBounds'] = gridBounds
+        gridSpecs['gridSpacing'] = gridSpacing
+        gridSpecs['gridExtent'] = gridExtent
+        gridSpecs['outputExtent'] = outputExtent
+        gridSpecs['gridShape'] = gridShape
+        gridSpecs['gridSize'] = gridSize
+        gridSpecs['md'] = md
+        if dset:  gridSpecs['attributes'] = dset.dataset.attributes
+        return gridSpecs
     
     def convert(self, cdms_var, **args):
         '''Convert a cdms data to vtk image data
