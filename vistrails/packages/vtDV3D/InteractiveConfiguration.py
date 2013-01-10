3b92ad01b6fb476f780648e8bd08ea0a328fdf59
Thomas Maxwell <thomas.maxwell@nasa.gov>
2013-01-10 16:11:50 -0500

    Resolve merge conflicts

--- vistrails/packages/vtDV3D/InteractiveConfiguration.py
+++ vistrails/packages/vtDV3D/InteractiveConfiguration.py
@@ -2376,7 +2376,7 @@ class AnimationConfigurationDialog( IVModuleConfigurationDialog ):
         if self.running: 
 #            delayTime = 0
             delayTime = ( self.maxSpeedIndex - self.speedSlider.value() + 1 ) * self.maxDelaySec * ( 1000.0 /  self.maxSpeedIndex )
-            print " Animate step %d, delay time = %.2f msec" % ( iTS, delayTime )
+ #           print " Animate step %d, delay time = %.2f msec" % ( iTS, delayTime )
             self.timer.start( delayTime ) 
 
     def finalizeConfig( self ):
