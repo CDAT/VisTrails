079234e09fb72662986dbf654d159b87bb3a8290
Thomas Maxwell <thomas.maxwell@nasa.gov>
2013-04-09 15:48:10 -0400

    Add support for Hydrax servers; Make ROI selection axis recognition more robust.

--- vistrails/packages/vtDV3D/CDMS_VariableReaders.py
+++ vistrails/packages/vtDV3D/CDMS_VariableReaders.py
@@ -42,7 +42,45 @@ def matchesAxisType( axis, axis_attr, axis_aliases ):
                 matches = True
                 break
     return matches
-    
+
+class AxisType:
+    NONE = 0
+    Time = 1
+    Longitude = 2
+    Latitude = 3
+    Level = 4
+    lev_aliases = [ 'bottom', 'top', 'zdim' ]
+    lev_axis_attr = [ 'z' ]
+    lat_aliases = [ 'north', 'south', 'ydim' ]
+    lat_axis_attr = [ 'y' ]
+    lon_aliases = [ 'east', 'west', 'xdim' ]
+    lon_axis_attr = [ 'x' ]
+
+def getAxisType( axis ):
+    if axis.isLevel() or matchesAxisType( axis, AxisType.lev_axis_attr, AxisType.lev_aliases ):
+        return AxisType.Level      
+    elif axis.isLatitude() or matchesAxisType( axis, AxisType.lat_axis_attr, AxisType.lat_aliases ):
+        return AxisType.Latitude                   
+    elif axis.isLongitude() or matchesAxisType( axis, AxisType.lon_axis_attr, AxisType.lon_aliases ):
+        return AxisType.Longitude     
+    elif axis.isTime():
+        return AxisType.Time
+    else: return  AxisType.NONE    
+
+def designateAxisType( self, axis ):
+    if not isDesignated( axis ):
+        if matchesAxisType( axis, AxisType.lev_axis_attr, AxisType.lev_aliases ):
+            axis.designateLevel() 
+            return AxisType.Level         
+        elif matchesAxisType( axis, AxisType.lat_axis_attr, AxisType.lat_aliases ):
+            axis.designateLatitude() 
+            return AxisType.Latitude                    
+        elif matchesAxisType( axis, AxisType.lon_axis_attr, AxisType.lon_aliases ):
+            axis.designateLongitude()
+            return AxisType.Longitude    
+    return getAxisType( axis )
+
+                   
 class PM_CDMSDataReader( PersistentVisualizationModule ):
     
     dataCache = {}
