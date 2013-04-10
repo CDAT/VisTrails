40ffc728eb1a4dd9b6643a17837cdc4af7f20609
Ben Burnett <bburnett@poly.edu>
2013-04-10 14:37:02 -0400

    Adds fix for transient variables

--- vistrails/packages/vtDV3D/PersistentModule.py
+++ vistrails/packages/vtDV3D/PersistentModule.py
@@ -1867,7 +1867,10 @@ class PersistentVisualizationModule( PersistentModule ):
             if len(ds.transientVariables)>0:
                 if len(ds.transientVariables)>1: print 'Warning: this module has several transient Variables, plotting the first one.'
                 var = ds.transientVariables.values()[0]
-                lensActor.SetTitle("Time vs. %s (%s)" % (var.long_name, var.id))
+                if hasattr(var, 'long_name'):
+                    lensActor.SetTitle("Time vs. %s (%s)" % (var.long_name, var.id))
+                else:
+                    lensActor.SetTitle("Time vs.%s" % var.id)
                 lensActor.SetYTitle(var.id)
                 lensActor.SetYRange(var.min(), var.max())
 
