ac041d673880d9e1b9d4519509e3c115fb722b2f
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-05-10 07:41:33 -0400

    Fixed merge conflict resolution

--- vistrails/packages/vtDV3D/PlotPipelineHelper.py
+++ vistrails/packages/vtDV3D/PlotPipelineHelper.py
@@ -1543,7 +1543,7 @@ class DV3DPipelineHelper( PlotPipelineHelper, QObject ):
         This method controls what config commands will be displayed in the DV3D configuration panel when a given plot is clicked upon.
         Config commands for all selected (blue rimmed) plots are displayed.   To display only the config commands for a single plot (type),
         deselect all rows and cols in the spreadsheet before clicking on a plot- this will select only plot that is clicked on.  The list
-        of selected cells is represented by the 'active_renwin_ids' list.  Config commands for plots in unselected cells have isActive=False 
+        of selected cells is represented by the 'active_cells' list.  Config commands for plots in unselected cells have isActive=False 
         and, as a result, are not displayed in the configuration panel.    
         
         """
