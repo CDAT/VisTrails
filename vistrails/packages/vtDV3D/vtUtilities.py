1f4ee8f55c4515a5335bdabd82ead95e7822ef9b
Thomas Maxwell <thomas.maxwell@nasa.gov>
2013-06-30 18:58:15 -0400

    Add memory logger

--- vistrails/packages/vtDV3D/vtUtilities.py
+++ vistrails/packages/vtDV3D/vtUtilities.py
@@ -59,48 +59,24 @@ currentTime = 0
 dvDbgIO = DebugPrint()
 dvDbgIO.set_stream( sys.stderr )
 
-import os
-_proc_status = '/proc/%d/status' % os.getpid()
-
-_scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
-          'KB': 1024.0, 'MB': 1024.0*1024.0}
-
-def _VmB(VmKey):
-    '''Private.
-    '''
-    global _proc_status, _scale
-     # get pseudo file  /proc/<pid>/status
-    try:
-        t = open(_proc_status)
-        v = t.read()
-        t.close()
-    except:
-        return 0.0  # non-Linux?
-     # get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
-    i = v.index(VmKey)
-    v = v[i:].split(None, 3)  # whitespace
-    if len(v) < 3:
-        return 0.0  # invalid format?
-     # convert Vm value to bytes
-    return float(v[1]) * _scale[v[2]]
-
-
-def memory(since=0.0):
-    '''Return memory usage in bytes.
-    '''
-    return _VmB('VmSize:') - since
-
-
-def resident(since=0.0):
-    '''Return resident memory usage in bytes.
-    '''
-    return _VmB('VmRSS:') - since
-
-
-def stacksize(since=0.0):
-    '''Return stack size in bytes.
-    '''
-    return _VmB('VmStk:') - since
+class MemoryLogger:
+    def __init__( self ):
+        self.logfile = None
+        
+    def __del__(self):
+        if self.logfile <> None: 
+            self.logfile.close( )
+            self.logfile = None
+        
+    def log( self, label ):
+        import resource
+        mem_usage = 4*resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000000.0
+        if self.logfile == None:
+            self.logfile = open( "/tmp/dv3d-memory_usage.log", 'w' )
+        self.logfile.write(" %10.2f: %s\n", mem_usage, label )
+        
+memoryLogger = MemoryLogger()        
+        
 def displayMessage( msg ):
     msgBox = QtGui.QMessageBox()
     msgBox.setText( msg )
