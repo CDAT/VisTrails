268248c2a66ae6bbf66da76c0adde1dded8ced47
Remi Rampin <remirampin@gmail.com>
2013-07-11 16:51:54 -0400

    Removes dependency cycle
    Although uvcdat_cdms really does import stuff from vtDV3D, remove the
    VisTrails dependency so that the program can start.

--- vistrails/packages/uvcdat_cdms/info.py
+++ vistrails/packages/uvcdat_cdms/info.py
@@ -3,4 +3,4 @@ version = '0.0.1'
 name = "UVCDAT CDMS"
 
 def package_dependencies():
-    return ['gov.llnl.uvcdat', 'edu.utah.sci.vistrails.spreadsheet', 'gov.nasa.nccs.vtdv3d']
+    return ['gov.llnl.uvcdat', 'edu.utah.sci.vistrails.spreadsheet']
