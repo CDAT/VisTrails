9cc2526e05d9393b4e305ade431c09e00b19d7ae
Thomas Maxwell <thomas.maxwell@nasa.gov>
2014-03-31 12:47:56 -0400

    Range fix

--- vistrails/packages/CPCViewer/MultiVarPointCollection.py
+++ vistrails/packages/CPCViewer/MultiVarPointCollection.py
@@ -547,8 +547,8 @@ class MultiVarPointCollection():
                         except TypeError, err:
                             pass
                     else:
-                        vmin = vrng[0]
-                        vmax = vrng[1]                      
+                        vmin = rmin
+                        vmax = rmax                  
                 if vmin <> None:
                     if ( threshold_target == 'z' ):
                         nLev = len( self.lev )
@@ -565,7 +565,6 @@ class MultiVarPointCollection():
         return None, None, None
                     
     def execute( self, args, **kwargs ): 
-        print "MultiVarPointCollection.execute: %s " % str( args ); sys.stdout.flush()
         op = args[0] 
         if op == 'indices': 
             threshold_mask = None
@@ -574,6 +573,7 @@ class MultiVarPointCollection():
                 var_data, vmin, vmax = self.computeThresholdRange( var_op )               
                 if not isNone(var_data):
                     var_mask = numpy.logical_and( numpy.greater_equal( var_data, vmin ), numpy.less_equal( var_data, vmax ) )  
+                    print "MultiVarPointCollection.execute: %s, mask range = %s  " % ( str( args ), str( (vmin, vmax) ) ); sys.stdout.flush()
                     if isNone(threshold_mask):                       
                         self.thresholdTargetType = 'coords' if var_op[0] in [ 'lat', 'lon', 'lev', 'x', 'y', 'z' ] else 'vardata' 
                         threshold_mask = var_mask
