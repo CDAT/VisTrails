1216f8830bef71f7fdd79b21a565fdb687409780
David Koop <dakoop@nyu.edu>
2014-04-03 23:10:36 -0700

    Make file types as separate entries

--- vistrails/packages/spreadsheet/spreadsheet_cell.py
+++ vistrails/packages/spreadsheet/spreadsheet_cell.py
@@ -349,7 +349,7 @@ class QCellToolBar(QtGui.QToolBar):
         cell = self.sheet.getCell(self.row, self.col)
         if isinstance(cell, QCellWidget):
             filename = QtGui.QFileDialog.getSaveFileName(
-                self, "Select a File to Export the Sheet",
+                self, "Save cell as...",
                 ".", ';;'.join(cell.save_formats))
             if filename:
                 filename = str(filename)
