0bef36437757fa01c5f0c449c18da3c612c15a47
David Koop <dakoop@nyu.edu>
2014-07-16 12:30:07 -0700

    Comment out self.window init

--- vistrails/packages/uvcdat_cdms/init.py
+++ vistrails/packages/uvcdat_cdms/init.py
@@ -1148,7 +1148,7 @@ class QCDATWidget(QVTKWidget):
     def __init__(self, parent=None):
         QVTKWidget.__init__(self, parent)
         #self.toolBarType = QCDATWidgetToolBar
-        self.window = None
+        # self.window = None
         self.canvas =  None
         #self.windowId = -1
         self.createCanvas()
