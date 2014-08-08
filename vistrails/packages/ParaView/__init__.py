3fad32425a39a735a08d0b3434c355f3bff17bd8
Remi Rampin <remirampin@gmail.com>
2014-08-08 13:42:01 -0400

    Fixes ParaView's package_requirements()

--- vistrails/packages/ParaView/__init__.py
+++ vistrails/packages/ParaView/__init__.py
@@ -11,6 +11,8 @@ def package_requirements():
     import core.requirements
     if not core.requirements.python_module_exists('vtk'):
         raise core.requirements.MissingRequirement('vtk')
+    if not core.requirements.python_module_exists('paraview'):
+        raise core.requirements.MissingRequirement('paraview')
     if not core.requirements.python_module_exists('PyQt4'):
         print 'PyQt4 is not available. There will be no interaction',
         print 'between VTK and the spreadsheet.'
