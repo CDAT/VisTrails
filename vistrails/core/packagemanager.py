CDAT/VisTrails

commit 447841f6e7638b12ea87dfc235264bde2d7697e9

Don't report MissingRequirements on first run

vistrails/core/packagemanager.py
@@ -46,6 +46,7 @@ import core.data_structures.graph
 import core.db.io
 from core.modules.module_registry import ModuleRegistry, MissingPackage
 from core.modules.package import Package
+from core.requirements import MissingRequirement
 from core.utils import VistrailsInternalError, InstanceObject, \
     versions_increasing
 ##############################################################################
@@ -524,6 +525,11 @@ Returns true if given package identifier is present."""
                 #pkg.check_requirements()
                 try:
                     self._registry.initialize_package(pkg)
+                except MissingRequirement, e:
+                    if report_missing_dependencies:
+                        debug.critical("Package <codepath %s> is missing a "
+                                       "requirement and will be disabled" %
+                                       pkg.codepath, str(e))
                 except Package.InitializationFailed, e:
                     debug.critical("Initialization of package <codepath %s> "
                                    "failed and will be disabled" % \

vistrails/core/modules/module_registry.py
@@ -46,6 +46,7 @@ import core.modules
 import core.modules.vistrails_module
 from core.modules.module_descriptor import ModuleDescriptor
 from core.modules.package import Package
+from core.requirements import MissingRequirement
 from core.utils import VistrailsInternalError, memo_method, \
      InvalidModuleClass, ModuleAlreadyExists, append_to_dict_of_lists, \
      all, profile, versions_increasing, InvalidPipeline
@@ -1470,6 +1471,8 @@ class ModuleRegistry(DBRegistry):
                     if hasattr(descriptor, 'module'):
                         self.auto_add_ports(descriptor.module)
                         added_descriptors.add(descriptor)
+        except MissingRequirement:
+            raise
         except Exception, e:
             raise package.InitializationFailed(package, 
                                                [traceback.format_exc()])

commit 2d66c90206347c6399a63412d7461e66b5ddffc5

Correctly handle MissingRequirement from packages
Bug introduced by 447841f6

vistrails/core/packagemanager.py
@@ -463,6 +463,10 @@ Returns true if given package identifier is present."""
                 # the reference in the package list
                 package.remove_own_dom_element()
                 failed.append(package)
+            except MissingRequirement, e:
+                debug.critical("Package <codepath %s> is missing a "
+                               "requirement: %s" % (
+                                   package.codepath, e.requirement), str(e))
             except Package.InitializationFailed, e:
                 debug.critical("Initialization of package <codepath %s> "
                                "failed and will be disabled" % \
@@ -531,8 +535,9 @@ Returns true if given package identifier is present."""
                 except MissingRequirement, e:
                     if report_missing_dependencies:
                         debug.critical("Package <codepath %s> is missing a "
-                                       "requirement and will be disabled" %
-                                       pkg.codepath, str(e))
+                                       "requirement: %s" %
+                                           (pkg.codepath, e.requirement),
+                                       str(e))
                     self.late_disable_package(pkg.codepath)
                 except Package.InitializationFailed, e:
                     debug.critical("Initialization of package <codepath %s> "
@@ -612,9 +617,8 @@ Returns true if given package identifier is present."""
                 pkg.load()
                 if pkg.identifier == identifier:
                     return pkg
-            except pkg.LoadFailed:
-                pass
-            except pkg.InitializationFailed:
+            except (pkg.LoadFailed, pkg.InitializationFailed,
+                    MissingRequirement):
                 pass
             except Exception, e:
                 pass

vistrails/gui/preferences.py
@@ -34,10 +34,12 @@
 
 from PyQt4 import QtGui, QtCore
 from core import get_vistrails_application
+from core.modules.package import Package
 from core.packagemanager import get_package_manager
 from core.utils import InvalidPipeline
 from core.utils.uxml import (named_elements,
                              elements_filter, enter_named_element)
+from core.requirements import MissingRequirement
 from gui.configuration import (QConfigurationWidget, QGeneralConfiguration,
                                QThumbnailConfiguration)
 from gui.module_palette import QModulePalette
@@ -312,7 +314,7 @@ class QPackagesWidget(QtGui.QWidget):
             palette.setUpdatesEnabled(False)
             try:
                 pm.late_enable_package(codepath)
-            except self._current_package.InitializationFailed, e:
+            except (Package.InitializationFailed, MissingRequirement), e:
                 debug.critical("Initialization of package '%s' failed" %
                                codepath, str(e))
                 raise